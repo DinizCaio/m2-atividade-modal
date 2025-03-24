fc6dd7ca5989628bf5a007628b9e810612fb7b68
Caio Diniz <dinizcaio97@gmail.com>
2025-03-24 17:21:11 -0300

    Funcões de abrir e fechar o modal adicionadas
    Função handleModal e função closeModal foram adicionadas

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,17 @@
 /* Desenvolva seu código aqui... */
+function handleModal() {
+  const link = document.querySelector(".form__register-link");
+  const modalContainer = document.querySelector(".modalController");
+  link.addEventListener("click", () => {
+    modalContainer.showModal();
+    closeModal();
+  });
+}
+handleModal();
+function closeModal() {
+  const modalContainer = document.querySelector(".modalController");
+  const closeButton = document.querySelector(".modal__close");
+  closeButton.addEventListener("click", () => {
+    modalContainer.close();
+  });
+}
